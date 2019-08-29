2b0b11bcd5c429e546a0f1ea8a44e1d9b6dc9f52
davidhw-unc <davidhw@live.unc.edu>
2019-08-29 12:47:49 -0400

    Wrote novice code

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,26 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		// Get # of customers
+		int customerCount = scan.nextInt();
+
+		// Loop through all customers
+		for (int i = 0; i < customerCount; i++) {
+			// Print cutomer name
+			System.out.printf("%c. %s: ", scan.next().charAt(0), scan.next());
+
+			// Find the total spent by the customer
+			int itemTypeCount = scan.nextInt();
+			double totalSpent = 0;
+			for (int j = 0; j < itemTypeCount; j++) {
+				int itemQuantity = scan.nextInt();
+				scan.next();
+				double itemPrice = scan.nextDouble();
+				totalSpent += itemQuantity * itemPrice;
+			}
+
+			// Print the total amount spent by the customer
+			System.out.printf("%.2f%n", totalSpent);
+		}
 	}
 }
