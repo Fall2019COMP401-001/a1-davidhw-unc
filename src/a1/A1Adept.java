5a4ef357a0b08c1f7a5e128534135f1f8af435fb
davidhw-unc <davidhw@live.unc.edu>
2019-08-29 13:39:39 -0400

    Wrote adept code

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,5 +1,7 @@
 package a1;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -8,7 +10,45 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		// Create a map relating the prices of items to their names
+		Map<String, Double> itemsSold = new HashMap<>();
+		int itemTypeCount = scan.nextInt();
+		for (int i = 0; i < itemTypeCount; i++) {
+			itemsSold.put(scan.next(), scan.nextDouble());
+		}
+
+		// Create variables to track relevant data
+		String bigFirst = "", bigLast = "", smallFirst = "", smallLast = "";
+		double bigSpent = 0, smallSpent = 0, overallTotalSpent = 0;
+		int numberOfCustomers = scan.nextInt();
+
+		// Loop through all customers
+		for (int i = 0; i < numberOfCustomers; i++) {
+			String first = scan.next(), last = scan.next();
+			int itemsBought = scan.nextInt();
+			double totalSpent = 0;
+
+			// Loop through each item purchased and add it to the customer's total
+			for (int j = 0; j < itemsBought; j++)
+				totalSpent += scan.nextInt() * itemsSold.get(scan.next());
+
+			// Update data tracker variables
+			overallTotalSpent += totalSpent;
+			if (bigFirst.equals("") || totalSpent > bigSpent) {
+				bigFirst = first;
+				bigLast = last;
+				bigSpent = totalSpent;
+			}
+			if (smallFirst.equals("") || totalSpent < smallSpent) {
+				smallFirst = first;
+				smallLast = last;
+				smallSpent = totalSpent;
+			}
+		}
+
+		// Print results
+		System.out.printf("Biggest: %s %s (%.2f)%n", bigFirst, bigLast, bigSpent);
+		System.out.printf("Smallest: %s %s (%.2f)%n", smallFirst, smallLast, smallSpent);
+		System.out.printf("Average: %.2f", overallTotalSpent / numberOfCustomers);
 	}
 }
